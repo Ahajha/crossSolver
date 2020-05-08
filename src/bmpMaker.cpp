fa895ce81f8af13498b5244adfe7166f0a6aa798
Alex <ahajha@gmail.com>
2020-05-08 09:46:57 -0400

    fix whitespace inconsistencies

--- src/bmpMaker.cpp
+++ src/bmpMaker.cpp
@@ -62,9 +62,9 @@ color_24& BMP_24::position(int x, int y)
 void BMP_24::write(const char* file) const
 {
 	std::ofstream out(file, std::ios_base::binary);
-
+	
 	out << *this;
-
+	
 	out.close();
 }
 
@@ -77,55 +77,54 @@ std::ostream& operator<<(std::ostream& out, const BMP_24& bmp)
 	// Size of the file // Used, only pixel size and amount will change this
 	BMP_24::writeLittleEndian(54 + bmp.height*((bmp.width*3)/4)*4 +
 		((bmp.width%4 != 0) * 4), out, 4);
-
-
+	
 	// Unused (x2)
 	BMP_24::writeLittleEndian(0, out, 4);
-
+	
 	// Start position of pixel array // Used, no features will change this
 	BMP_24::writeLittleEndian(54, out, 4);
-
+	
 	/* DIB Header */
-
+	
 	// Number of bytes in the header // Used, no features will change this
 	BMP_24::writeLittleEndian(40, out, 4);
-
+	
 	// Width of the bitmap in pixels (left to right) // Used
 	BMP_24::writeLittleEndian(bmp.width, out, 4);
-
+	
 	// Height of the bitmap in pixels (bottom to top) // Used
 	BMP_24::writeLittleEndian(bmp.height, out, 4);
-
+	
 	// Number of color planes used // No feature for this
 	out << (char)1;
 	out << (char)0;
-
+	
 	// Number of bits per pixel // No feature for this
 	out << (char)24;
 	out << (char)0;
 
 	// Compression type // No feature for this
 	BMP_24::writeLittleEndian(0, out, 4);
-
+	
 	// Size of raw bitmap data in bytes (including padding) // Must be used
 	BMP_24::writeLittleEndian(bmp.height*((bmp.width*3)/4)*4 + ((bmp.width%4 != 0) * 4), out, 4);
-
+	
 	// Print resolution:
-
+	
 	// p/m (horizontal) // No feature for this
 	BMP_24::writeLittleEndian(2835, out, 4);
-
+	
 	// p/m (vertical) // No feature for this
 	BMP_24::writeLittleEndian(2835, out, 4);
-
+	
 	// Number of colors in the palette // No feature for this
 	BMP_24::writeLittleEndian(0, out, 4);
-
+	
 	// Important colors (0 means all) // No feature for this
 	BMP_24::writeLittleEndian(0, out, 4);
-
+	
 	/* Pixel Array */
-
+	
 	int padding = (bmp.width % 4 == 0) ? 0 : (bmp.width % 4);
 	
 	// From the bottom left, going row by row.
--- src/bmpMaker.h
+++ src/bmpMaker.h
@@ -19,22 +19,22 @@ A color_24 is a 24 bit color. Contains BGR.
 struct color_24
 {
 	char B, G, R;
-
+	
 	color_24() : B(DEFAULT_B), G(DEFAULT_G), R(DEFAULT_R) {}
-
+	
 	color_24(char b, char g, char r) : B(b), G(g), R(r) {}
-
+	
 	// Constants
 	static const color_24 white;
 	static const color_24 light_gray;
 	static const color_24 gray;
 	static const color_24 dark_gray;
 	static const color_24 black;
-
+	
 	static const color_24 red;
 	static const color_24 blue;
 	static const color_24 green;
-
+	
 	static const color_24 yellow;
 	static const color_24 pink;
 	static const color_24 light_blue;
@@ -42,7 +42,7 @@ struct color_24
 	static const color_24 orange;
 	static const color_24 light_brown;
 	static const color_24 magenta;
-		
+	
 	friend std::ostream& operator<<(std::ostream& out, const color_24& col);
 };
 
@@ -53,20 +53,20 @@ BMP_24 can be used to make or modify 24-bit BMP files.
 class BMP_24
 {
 	private:
-
+	
 	int width;
 	int height;
-
+	
 	// Row 0 is the bottom row, column 0 is the left
 	std::vector<std::vector<color_24>> grid;
-
+	
 	static void writeLittleEndian(int e, std::ostream& out, int numBytes);
-
+	
 	public:
-
+	
 	// Creates a bitmap of specified size (default white)
 	BMP_24(int hgt, int wid);
-
+	
 	// Creates a bitmap of specified size and default color
 	BMP_24(int hgt, int wid, color_24 def);
 	
@@ -74,9 +74,9 @@ class BMP_24
 	// BMP_24(const char* file);
 
 	color_24& position(int x, int y);
-
+	
 	friend std::ostream& operator<<(std::ostream& out, const BMP_24& bmp);
-		
+	
 	void write(const char* file) const;
 };
 
